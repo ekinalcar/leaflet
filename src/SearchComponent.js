7c07e05721eb2d71271b7e6490b6f5bf7c66c877
Ekin Alçar <ekinalcar@users.noreply.github.com>
2020-03-06 15:54:07 +0100

    initial commit

---
+++ src/SearchComponent.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactLeafletSearch from "react-leaflet-search";
+import { Popup } from "react-leaflet";
+
+const SearchComponent = props => {
+  const myPopup = SearchInfo => {
+    return (
+      <Popup>
+        <div>
+          <p>I am a custom popUp</p>
+          <p>
+            latitude and longitude from search component: lat:
+            {SearchInfo.latLng["lat"]} lng:{SearchInfo.latLng["lng"]}
+          </p>
+          <p>Info from search component: {SearchInfo.info}</p>
+          <button
+            type="button"
+            onClick={event => props.clickHandler(event, SearchInfo)}
+          >
+            Click me
+          </button>
+        </div>
+      </Popup>
+    );
+  };
+  return (
+    <ReactLeafletSearch
+      position="topleft"
+      provider="OpenStreetMap"
+      popUp={myPopup}
+    />
+  );
+};
+
+export default SearchComponent;
