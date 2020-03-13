e914a8303691820fe075042ddf41387bf1104283
Ekin Alçar <ekinalcar@users.noreply.github.com>
2020-03-13 13:50:09 +0100

    added express backend and client

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
 import { Route, Switch, withRouter, Redirect } from "react-router-dom";
-
+import axios from "axios";
 import Header from "./containers/Layout/Header";
 import Footer from "./containers/Layout/Footer";
 import Map from "./containers/Layout/Map";
 
 class App extends Component {
+  componentDidMount = async () => {
+    const response = await this.callApi();
+    console.log(response);
+  };
+  callApi = async () => {
+    const response = await axios("/api/hello");
+    if (response.status !== 200) throw Error(response.message);
+
+    return response;
+  };
   render() {
     return (
       <>
