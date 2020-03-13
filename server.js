f5936d4091f765c350cea1b6a11ebba8472a6638
Ekin Alçar <ekinalcar@users.noreply.github.com>
2020-03-13 14:00:07 +0100

    added node packages to server

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const mongoSanitize = require("express-mongo-sanitize");
 const bodyParser = require("body-parser");
 
 const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+});
+app.use(limiter);
+app.use(helmet());
+
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(hpp());
+app.use(mongoSanitize());
 
 app.get("/api/hello", (req, res) => {
   res.send({ express: "Hello From Express" });
