21d4fbe9a36f4013930bb161f10230f984f863fc
Ekin Alçar <ekinalcar@gmail.com>
2020-09-10 14:36:50 +0200

    fixed action types

--- client/src/redux/actions/types.js
+++ client/src/redux/actions/types.js
@@ -1,6 +1,6 @@
-export const LOCATION_START = "LOCATION_START";
-export const LOCATION_END = "LOCATION_END";
-export const LOCATION_SUCCESS = "LOCATION_SUCCESS";
-export const LOCATION_FAIL = "LOCATION_FAIL";
+export const LOCATION_START = "@locations/LOCATION_START";
+export const LOCATION_END = "@locations/LOCATION_END";
+export const LOCATION_SUCCESS = "@locations/LOCATION_SUCCESS";
+export const LOCATION_FAIL = "@locations/LOCATION_FAIL";
 
-export const SAVE_LOCATION_SUCCESS = "SAVE_LOCATION_SUCCESS";
+export const SAVE_LOCATION_SUCCESS = "@locations/SAVE_LOCATION_SUCCESS";
--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,10 +1,6 @@
 import { combineReducers } from "redux";
 import locationReducer from "../reducers/location";
 
-/*export default combineReducers({
-  locations: locationReducer,
-});
-*/
 export const rootReducer = combineReducers({
   locations: locationReducer,
 });
